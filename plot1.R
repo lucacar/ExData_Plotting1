6d6573538f01ae9931cf3e59efdb75cabf576134
lucacar <luke19650217@gmail.com>
2015-08-03 16:42:26 -0500

    commit plot 1

--- plot1.R
+++ plot1.R
@@ -1,22 +1,27 @@
 plot1 <- function() {
+
+  plotGraph <- function() {
+    # Generating Plot 1
+    hist(barplots$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)")
+  }
   
-if(!file.exists("household_power_consumption.txt")) {
-  temp <- tempfile()
-  download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
-  file <- unzip(temp)
-  unlink(temp)
-}
-
-HouseholdFile <- file("household_power_consumption.txt")
-barplots  <- read.table(text = grep("^[1,2]/2/2007", readLines(HouseholdFile), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", header = TRUE)
-
-# Generating Plot 1
-hist(barplots$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)")
-
-# Generating Plot png file
-dev.copy(png, file="plot1.png", width=480, height=480)
-dev.off()
-cat("Plot1.png has been saved in", getwd())
+  if(!file.exists("household_power_consumption.txt")) {
+    temp <- tempfile()
+    download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+    file <- unzip(temp)
+    unlink(temp)
+  }
+  
+  HouseholdFile <- file("household_power_consumption.txt")
+  barplots  <- read.table(text = grep("^[1,2]/2/2007", readLines(HouseholdFile), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", header = TRUE)
+  
+  plotGraph()
+  
+  # Generating Plot png file
+  png("plot1.png", width=480, height=480)
+  plotGraph()
+  dev.off()
+  cat("Plot1.png has been saved in", getwd())
 }
 
 plot1()

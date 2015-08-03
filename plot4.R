139c67a65af7a21c129126aa33207626e0f96fb4
lucacar <luke19650217@gmail.com>
2015-08-03 16:44:58 -0500

    commit plot 4

--- plot4.R
+++ plot4.R
@@ -1,5 +1,21 @@
 plot4 <- function() {
   
+  plotGraph <- function() {
+    par(mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
+    with(data, {
+      plot(Global_active_power ~ Datetime, type = "l", 
+           ylab = "Global Active Power", xlab = "")
+      plot(Voltage ~ Datetime, type = "l", ylab = "Voltage", xlab = "datetime")
+      plot(Sub_metering_1 ~ Datetime, type = "l", ylab = "Energy sub metering",
+           xlab = "")
+      lines(Sub_metering_2 ~ Datetime, col = 'Red')
+      lines(Sub_metering_3 ~ Datetime, col = 'Blue')
+      legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), lwd=c(1,1))
+      plot(Global_reactive_power ~ Datetime, type = "l", 
+           ylab = "Global_rective_power", xlab = "datetime")
+    })
+  }
+  
   if(!file.exists("household_power_consumption.txt")) {
     temp <- tempfile()
     download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
@@ -23,22 +39,11 @@ plot4 <- function() {
   data$Datetime <- as.POSIXct(datetime)
   
   ## Generating Plot 4
-  par(mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
-  with(data, {
-    plot(Global_active_power ~ Datetime, type = "l", 
-         ylab = "Global Active Power", xlab = "")
-    plot(Voltage ~ Datetime, type = "l", ylab = "Voltage", xlab = "datetime")
-    plot(Sub_metering_1 ~ Datetime, type = "l", ylab = "Energy sub metering",
-         xlab = "")
-    lines(Sub_metering_2 ~ Datetime, col = 'Red')
-    lines(Sub_metering_3 ~ Datetime, col = 'Blue')
-    legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), lwd=c(1,1))
-    plot(Global_reactive_power ~ Datetime, type = "l", 
-         ylab = "Global_rective_power", xlab = "datetime")
-  })
+  plotGraph()
   
   ## Copying plot to png file
-  dev.copy(png, file="plot4.png", width=480, height=480)
+  png("plot4.png", width=480, height=480)
+  plotGraph()
   dev.off()
   ## Output directory where the png file was saved
   cat("plot4.png has been saved in", getwd())
